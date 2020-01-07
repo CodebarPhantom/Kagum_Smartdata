3e0cf635a98b92252cf599c14c1f228abb5f7ff8
Eryan Fauzan Home <eryangamer10@gmail.com>
2020-01-07 20:11:59 +0700

    Add Phpspreadsheet

--- application/views/smartreport/actual_pnl.php
+++ application/views/smartreport/actual_pnl.php
@@ -1135,8 +1135,8 @@ function cal_days_in_year($yearact){
 												<td class='rata-kanan'> 
 													<strong>
 														<?php
-															if($grandtotal_und_exp_actual !=0 && $grandtotal_totalsales_actual->GRANDTOTAL_PNLCATEGORY != 0){
-																echo number_format(($grandtotal_und_exp_actual/$grandtotal_totalsales_actual->GRANDTOTAL_PNLCATEGORY)*100,2).'%';
+															if($grandtotal_und_exp_actual !=0 && $grandtotal_totalsalesactual->GRANDTOTAL_PNLCATEGORY != 0){
+																echo number_format(($grandtotal_und_exp_actual/$grandtotal_totalsalesactual->GRANDTOTAL_PNLCATEGORY)*100,2).'%';
 															}else{
 																echo '0%';
 															}															

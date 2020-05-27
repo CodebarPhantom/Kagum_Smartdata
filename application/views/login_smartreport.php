81c53293704c4fcedeb02b8f849f33527534bf60
Eryan Fauzan Home <eryangamer10@gmail.com>
2020-05-27 14:35:35 +0700

    no index google login

--- application/views/login_smartreport.php
+++ application/views/login_smartreport.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="robots" content="noindex"/>
+	<meta name="googlebot" content="noindex"/>
+	<meta name="robots" content="noindex, nofollow"/>
 	<title>Kagum Hotels Smart Report </title>
 
 	<!-- Global stylesheets -->
--- application/views/smartreport/includes_top.php
+++ application/views/smartreport/includes_top.php
@@ -1,6 +1,4 @@
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="robots" content="noindex"/>
 	<meta name="googlebot" content="noindex"/>
 	<meta name="robots" content="noindex, nofollow"/>
